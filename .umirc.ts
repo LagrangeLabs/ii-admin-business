01f9b47bfab27076d2cc02a618c760bb40877928
大黄蜂(张明瑞) <zhangmingrui@mail.i-i.ai>
2020-10-27 10:13:42 +0800

    fix: 刷新页面404问题

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'ii-ui-business',
+  title: 'ii-admin-business',
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  publicPath: '/ii-admin-business/',
+  base: '/ii-admin-business',
+  history: {type: 'hash'},
   // more config: https://d.umijs.org/config
 });
